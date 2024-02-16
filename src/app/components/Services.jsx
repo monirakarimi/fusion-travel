fa5fdb9e165149b6ce1136347722cea52f840900
monirakarimi <monirakarimi.mk@gmail.com>
2024-02-16 19:47:18 +0430

    feat(core): add slider to services section

--- src/app/components/Services.jsx
+++ src/app/components/Services.jsx
@@ -1,30 +1,82 @@
-// // Import Swiper React components
-// import { Swiper, SwiperSlide } from 'swiper/react';
+"use client"
+import aboutimg1 from "@/app/images/aboutimg1.jpg"
+import aboutimg2 from "@/app/images/aboutimg2.jpg"
+import aboutimg3 from "@/app/images/aboutimg3.jpg"
+import aboutimg4 from "@/app/images/aboutimg4.jpg"
+import aboutimg5 from "@/app/images/aboutimg5.jpg"
+import aboutimg6 from "@/app/images/aboutimg6.jpg"
+import Image from "next/image"
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+const img = [
+  {
+    id: 1, 
+    imageUrl: aboutimg1,
+  },
+  {
+    id: 2,
+    imageUrl: aboutimg2,
+  },
+  {
+    id: 3,
+    imageUrl: aboutimg3,
+  },
+  {
+    id: 4, 
+    imageUrl: aboutimg4,
+  },
+  {
+    id: 5,
+    imageUrl: aboutimg5,
+  },
+  {
+    id: 6,
+    imageUrl: aboutimg6,
+  },
+]
 
-// // Import Swiper styles
-// import 'swiper/css';
-// import 'swiper/react';
-
-export default function Services() {
+export default function Blog() {
   return (
     <div className="px-6 lg:px-8">
+      
       <div className="mx-auto max-w-2xl text-center">
-        <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">Find Your Destination</h2>
-        <p className="mt-6 text-lg leading-8 text-gray-300">
+        <h2 className="text-3xl font-bold tracking-tight sm:text-4xl">
+          Find Your Destination
+        </h2>
+        <p className="mt-2 text-lg leading-8 text-gray-300">
           Most Popular Places Are Waiting For You
         </p>
       </div>
-
-      <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-6 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-8">
-        {/* <Swiper
-        spaceBetween={50}
-        slidesPerView={3}
+      <Swiper
+          spaceBetween={50}
+          slidesPerView={3}
+          onSlideChange={() => console.log('slide change')}
+          onSwiper={(swiper) => console.log(swiper)}
+          className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3"
         >
-          <SwiperSlide>Slide 1</SwiperSlide>
-          <SwiperSlide>Slide 2</SwiperSlide>
-          <SwiperSlide>Slide 3</SwiperSlide>
-        </Swiper> */}
-      </div>
+       
+          {img.map((i) => (
+            <SwiperSlide key={i.id} className="flex flex-col items-start justify-between">
+            {/* <article key={i.id} className="flex flex-col items-start justify-between"> */}
+              
+              <div className="relative w-full">
+                
+                  <Image
+                    src={i.imageUrl}
+                    alt=""
+                    className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
+                  />
+                  <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
+                
+              </div>
+              
+            {/* </article> */}
+            </SwiperSlide>
+          ))}
+          
+        
+      {/* </div>   */}
+      </Swiper> 
     </div>
   )
 }
