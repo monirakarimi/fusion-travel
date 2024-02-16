532f406f0ef8a6640256e59084164678e56386c3
monirakarimi <monirakarimi.mk@gmail.com>
2024-02-16 21:18:16 +0430

    feat(common): add id to about section

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -18,7 +18,7 @@ const cards = [
 ]
 export default function About() {
   return (
-    <div className="px-6 py-24 sm:py-32 lg:px-8">
+    <div id="about" className="px-6 py-24 sm:py-32 lg:px-8">
       <div className="mx-auto max-w-2xl text-center">
         <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">Welcome to Fusion</h2>
         <p className="mt-6 text-lg leading-8 text-gray-300">
